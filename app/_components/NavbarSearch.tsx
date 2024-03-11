3013644e7ea5c640fd502ef184f6a97dcc6cd9de
Maciej Mądry <maciejmadry0@gmail.com>
2024-03-11 11:15:03 +0100

    FIX: updated navbar seatch to search for product brands

--- app/_components/NavbarSearch.tsx
+++ app/_components/NavbarSearch.tsx
@@ -28,7 +28,7 @@ const NavbarSearch = () => {
   const debouncedSearchValue = useDebounce(searchValue, 1000);
 
   useEffect(() => {
-    if (debouncedSearchValue.length > 3) {
+    if (debouncedSearchValue.length >= 3) {
       setIsLoading(true);
       setSearchResult(null);
       searchProducts(debouncedSearchValue)
@@ -66,12 +66,14 @@ const NavbarSearch = () => {
                 <Loader2 size={25} className="animate-spin" />
               </div>
             )}
-            {!isLoading && !searchResult && debouncedSearchValue.length > 3 && (
-              <p className="mt-3 p-2 text-center text-sm">No results.</p>
-            )}
-            {debouncedSearchValue.length < 4 && (
+            {!isLoading &&
+              !searchResult &&
+              debouncedSearchValue.length >= 3 && (
+                <p className="mt-3 p-2 text-center text-sm">No results.</p>
+              )}
+            {debouncedSearchValue.length < 3 && (
               <p className="mt-3 p-2 text-center text-sm">
-                The search value must be longer than 3 characters.
+                The search value must be at least 3 characters.
               </p>
             )}
 
@@ -84,8 +86,13 @@ const NavbarSearch = () => {
                         {searchResult.productsByName?.map(
                           ({ name, id, slug }) => {
                             return (
-                              <CommandItem key={id}>
-                                <Link href={`/product/${slug}`}>{name}</Link>
+                              <CommandItem
+                                key={id}
+                                onMouseDown={() => setOpen(false)}
+                              >
+                                <Link href={`/product/${encodeURI(slug)}`}>
+                                  {name}
+                                </Link>
                               </CommandItem>
                             );
                           },
@@ -103,43 +110,33 @@ const NavbarSearch = () => {
                       <hr />
                     </>
                   )}
-                {searchResult.productsByDescription &&
-                  searchResult.productsByDescription.length > 0 && (
-                    <>
-                      <CommandGroup heading="Product descriptions matching your search:">
-                        {searchResult.productsByDescription?.map(
-                          ({ name, id, description, slug }) => {
-                            return (
-                              <div className="my-1" key={id}>
-                                <CommandItem>
-                                  <Link href={`/product/${slug}`}>{name}</Link>
-                                </CommandItem>
-                                <p className="px-2 py-1.5 text-xs">
-                                  {description}
-                                </p>
-                                <hr className="opacity-40" />
-                              </div>
-                            );
-                          },
-                        )}
-                      </CommandGroup>
-                      {searchResult.productsByDescriptionCounter! >
-                        searchResult.productsByDescription.length && (
-                        <Link href={`/products?description=${searchValue}`}>
-                          <p className="px-2 py-1.5 text-xs font-semibold underline">
-                            See all products whose description matches your
-                            search - {searchResult.productsByDescriptionCounter}
-                          </p>
-                        </Link>
-                      )}
-                    </>
-                  )}
+
+                {searchResult.brands && searchResult.brands.length > 0 && (
+                  <>
+                    <CommandGroup heading="Product brands matching your search:">
+                      {searchResult.brands?.map((brand) => {
+                        return (
+                          <CommandItem
+                            key={brand}
+                            onMouseDown={() => setOpen(false)}
+                          >
+                            <Link href={`/products?brand=${encodeURI(brand)}`}>
+                              {brand}
+                            </Link>
+                          </CommandItem>
+                        );
+                      })}
+                    </CommandGroup>
+                    <hr />
+                  </>
+                )}
+
                 {searchResult.category && (
                   <>
                     <CommandGroup heading="Category:">
-                      <CommandItem>
+                      <CommandItem onMouseDown={() => setOpen(false)}>
                         <Link
-                          href={`/products?category=${searchResult.category}`}
+                          href={`/products?category=${encodeURI(searchResult.category)}`}
                         >
                           {searchResult.category}
                         </Link>
--- app/actions.ts
+++ app/actions.ts
@@ -2,15 +2,13 @@
 
 import { Prisma, Product } from "@prisma/client";
 import prisma from "@/lib/prisma";
-import { truncateString } from "@/lib/utils";
 import { signOut } from "@/auth";
 
 export interface SearchResult {
   productsByName?: Product[];
   productsByNameCounter?: number;
-  productsByDescription?: Product[];
-  productsByDescriptionCounter?: number;
   category?: string;
+  brands?: string[];
 }
 
 export const searchProducts = async (
@@ -18,51 +16,39 @@ export const searchProducts = async (
 ): Promise<SearchResult | null> => {
   const where = (key: keyof Product): Prisma.ProductWhereInput => ({
     [key]: { contains: query, mode: "insensitive" },
+    approved: true,
   });
 
-  const [
-    productsByName,
-    productsByNameCounter,
-    productsByDescription,
-    productsByDescriptionCounter,
-    cat,
-  ] = await Promise.all([
+  const [productsByName, productsByNameCounter, cat, brnd] = await Promise.all([
     prisma.product.findMany({
       where: where("name"),
       take: 4,
     }),
     prisma.product.count({ where: where("name") }),
-    prisma.product.findMany({
-      where: where("description"),
-      take: 2,
-    }),
-    prisma.product.count({
-      where: where("description"),
-    }),
     prisma.product.findFirst({
       where: {
         category: { contains: query, mode: "insensitive" },
       },
     }),
+    prisma.product.findMany({
+      where: where("brand"),
+      distinct: ["brand"],
+      select: { brand: true },
+    }),
   ]);
 
-  if (!productsByName.length && !productsByDescription.length && !cat) {
+  if (!productsByName.length && !brnd.length && !cat) {
     return null;
   }
 
   const category = cat?.category.split(",").find((ctg) => ctg.includes(query));
+  const brands = brnd.map((b) => b.brand);
 
   return {
     productsByName,
     productsByNameCounter,
-    productsByDescription: productsByDescription
-      .filter((prod) => !productsByName.some((p) => p.id === prod.id))
-      .map((prod) => ({
-        ...prod,
-        description: truncateString(prod.description, query, 10),
-      })),
-    productsByDescriptionCounter,
     category: category && `${category[0].toUpperCase()}${category.slice(1)}`,
+    brands,
   };
 };
 

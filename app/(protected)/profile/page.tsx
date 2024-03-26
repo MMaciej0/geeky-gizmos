0e4a24344681f164dabe1875ad4b1a9f097b1610
Maciej Mądry <maciejmadry0@gmail.com>
2024-03-26 19:09:44 +0100

    ADD: profile page placeholder

--- app/(protected)/profile/page.tsx
+++ app/(protected)/profile/page.tsx
@@ -3,7 +3,14 @@ import { getUser } from "@/lib/utils";
 
 const ProfilePage = async () => {
   const user = await getUser();
-  return <div>{JSON.stringify(user)}</div>;
+  return (
+    <div className="py-10 ">
+      <div className="text-center">
+        <h1 className="text-center text-3xl">Hello {user?.name}</h1>
+        <p>Thank you for visitig this page.</p>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;

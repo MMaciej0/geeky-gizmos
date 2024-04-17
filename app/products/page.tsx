20ecbd4970e0fe00d527c80d78bbdec1ff6cf54a
Maciej Mądry <83778733+MMaciej0@users.noreply.github.com>
2024-04-17 19:30:31 +0200

    Revert "ADD: inifinite scrolling to products page"

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,17 +1,30 @@
-import { Suspense } from "react";
-import { nanoid } from "nanoid";
+import Link from "next/link";
 
-import {
-  SearchParams,
-  fetchBrands,
-  fetchCategories,
-  fetchProducts,
-} from "./actions";
+import prisma from "@/lib/prisma";
 
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import ProductCard from "@/components/ProductCard";
 import FilterPanel from "./_components/FilterPanel";
+import { Suspense } from "react";
 import ProductsLoadingSkeleton from "@/components/ProductsLoadingSkeleton";
+import Await from "@/components/Await";
 import ProductsList from "./_components/ProductsList";
+import { ProductWithBrandPayload } from "@/types/product";
+import { nanoid } from "nanoid";
+
+interface SortOptions {
+  newest: { key: string; method: "desc" };
+  oldest: { key: string; method: "asc" };
+  cheapest: { key: string; method: "asc" };
+  expensive: { key: string; method: "desc" };
+}
+
+export interface SearchParams {
+  category?: string | string[];
+  name?: string;
+  brand?: string | string[];
+  sort?: keyof SortOptions;
+}
 
 interface ProductsPageProps {
   searchParams: SearchParams;
@@ -34,7 +47,11 @@ const ProductsPage = async ({ searchParams }: ProductsPageProps) => {
       </div>
       <div key={nanoid(10)} className="w-full">
         <Suspense fallback={<ProductsLoadingSkeleton qty={9} />}>
-          <Products searchParams={searchParams} />
+          <Await promise={fetchProducts(searchParams)}>
+            {(products: ProductWithBrandPayload[]) => (
+              <ProductsList products={products} />
+            )}
+          </Await>
         </Suspense>
       </div>
     </MaxWidthWrapper>
@@ -43,10 +60,155 @@ const ProductsPage = async ({ searchParams }: ProductsPageProps) => {
 
 export default ProductsPage;
 
-const Products = async ({ searchParams }: ProductsPageProps) => {
-  const products = await fetchProducts(searchParams);
+const sortCheatSheet: SortOptions = {
+  newest: { key: "createdAt", method: "desc" },
+  oldest: { key: "createdAt", method: "asc" },
+  cheapest: { key: "price", method: "asc" },
+  expensive: { key: "price", method: "desc" },
+};
 
-  return (
-    <ProductsList initialProducts={products} searchParams={searchParams} />
-  );
+const fetchProducts = async (searchParams: SearchParams) => {
+  const { category, name, brand, sort } = searchParams;
+  const orderBy = sort
+    ? { [sortCheatSheet[sort].key]: sortCheatSheet[sort].method }
+    : {};
+  const products = await prisma.product.findMany({
+    where: {
+      categories: Array.isArray(category)
+        ? {
+            some: {
+              category: {
+                name: {
+                  in: category,
+                },
+              },
+            },
+          }
+        : category
+          ? {
+              some: {
+                category: {
+                  name: {
+                    contains: category,
+                    mode: "insensitive",
+                  },
+                },
+              },
+            }
+          : {},
+      brand: Array.isArray(brand)
+        ? {
+            is: {
+              name: {
+                in: brand,
+              },
+            },
+          }
+        : brand
+          ? {
+              is: {
+                name: {
+                  contains: brand,
+                  mode: "insensitive",
+                },
+              },
+            }
+          : {},
+      name: name
+        ? {
+            contains: name,
+            mode: "insensitive",
+          }
+        : {},
+    },
+    include: {
+      brand: true,
+    },
+    orderBy,
+  });
+  return products;
+};
+
+const fetchBrands = async (searchParams: SearchParams) => {
+  const { category } = searchParams;
+
+  const brands = await prisma.brand.findMany({
+    where: {
+      products: Array.isArray(category)
+        ? {
+            some: {
+              categories: {
+                some: {
+                  category: {
+                    name: {
+                      in: category,
+                    },
+                  },
+                },
+              },
+            },
+          }
+        : category
+          ? {
+              some: {
+                categories: {
+                  some: {
+                    category: {
+                      name: {
+                        contains: category,
+                        mode: "insensitive",
+                      },
+                    },
+                  },
+                },
+              },
+            }
+          : {},
+    },
+    include: { products: true },
+  });
+  return brands;
+};
+
+const fetchCategories = async (searchParams: SearchParams) => {
+  const { brand } = searchParams;
+
+  const categories = await prisma.category.findMany({
+    where: {
+      products: Array.isArray(brand)
+        ? {
+            some: {
+              product: {
+                is: {
+                  brand: {
+                    name: {
+                      in: brand,
+                    },
+                  },
+                },
+              },
+            },
+          }
+        : brand
+          ? {
+              some: {
+                product: {
+                  is: {
+                    brand: {
+                      name: {
+                        contains: brand,
+                        mode: "insensitive",
+                      },
+                    },
+                  },
+                },
+              },
+            }
+          : {},
+    },
+    orderBy: {
+      name: "asc",
+    },
+  });
+  return categories;
 };
